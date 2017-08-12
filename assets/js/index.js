b6cac4ccd4bf886f6a183fce9a318f0f07a10c80
Jieun Rim <rim.jieun@gmail.com>
2017-08-12 02:05:14 -0400

    wip

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,65 @@
-$(document).ready(function() {
+ // var center = {lat: -25.363, lng: 131.044};
 
-});
+
+
+// function initMap() {
+  
+//   console.log("initmap");
+  
+
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: center
+//   });
+
+//   var marker = new google.maps.Marker({
+//     draggable: true,
+//     animation: google.maps.Animation.DROP,
+//     position: center,
+//     map: map
+//   });
+
+//   google.maps.event.addListener(marker, 'dragend', function(marker){
+//     var latLng = marker.latLng; 
+//     currentLatitude = latLng.lat();
+//     currentLongitude = latLng.lng();
+//     console.log(currentLatitude, currentLongitude);
+//  }); 
+// }
+
+var center = {lat: 33.902589225, lng: -84.369156975};
+
+ // var center = {lat: -25.363, lng: 131.044};
+function initMap(center) {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: center
+  });
+
+  var marker = new google.maps.Marker({
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: center,
+      map: map
+    });
+
+  google.maps.event.addListener(marker, 'dragend', function(marker){
+    var latLng = marker.latLng; 
+    currentLatitude = latLng.lat();
+    currentLongitude = latLng.lng();
+    console.log(currentLatitude, currentLongitude);
+  }); 
+}
+
+initMap(center);
+
+
+$("#submit-btn").on("click", function(e) {
+    e.preventDefault();
+
+    center.lat = Number(document.getElementById("lat").value);
+    center.lng = Number(document.getElementById("lng").value);
+
+    initMap(center);
+
+  });
