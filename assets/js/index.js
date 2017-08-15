da2855e9fbdb39b29207df1a5223478e535f5d41
Jieun Rim <rim.jieun@gmail.com>
2017-08-15 15:53:55 -0400

    add person done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,11 @@
- // var center = {lat: -25.363, lng: 131.044};
+(function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(storePosition);
+  } else { 
+      console.log('Geolocation is not supported by this browser.');
+  }
+})();
 
-
-
-// function initMap() {
-  
-//   console.log("initmap");
-  
-
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 4,
-//     center: center
-//   });
-
-//   var marker = new google.maps.Marker({
-//     draggable: true,
-//     animation: google.maps.Animation.DROP,
-//     position: center,
-//     map: map
-//   });
-
-//   google.maps.event.addListener(marker, 'dragend', function(marker){
-//     var latLng = marker.latLng; 
-//     currentLatitude = latLng.lat();
-//     currentLongitude = latLng.lng();
-//     console.log(currentLatitude, currentLongitude);
-//  }); 
-// }
-
-var center = {lat: 33.902589225, lng: -84.369156975};
-
- // var center = {lat: -25.363, lng: 131.044};
 function initMap(center) {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
@@ -54,26 +30,44 @@ function initMap(center) {
   }); 
 }
 
-initMap(center);
+function storePosition(position) {
+  var center = {
+    lat: Number(position.coords.latitude),
+    lng: Number(position.coords.longitude)
+  };
+  initMap(center);
+}
 
+var autocomplete = new google.maps.places.Autocomplete(document.getElementById('address-input'));
 
-$("#submit-btn").on("click", function(e) {
-    e.preventDefault();
+function Person(name, add, noti, method, cont) {
+  this.name = name;
+  this.address = add;
+  this.wantNotification = noti;
+  this.notificationMethod = method;
+  this.contactInfo = cont;
+}
 
-    center.lat = Number(document.getElementById("lat").value);
-    center.lng = Number(document.getElementById("lng").value);
+function addPerson(name, address) {
+  var newPersonItem = $('<li class="person" data-name="' + name + '" data-address="' + address + '"><div class="person-icon left"><i class="fa fa-user" aria-hidden="true"></i></div><div class="person-info left">' + name + '<p class="person-address">' + address + '</p></div><div class="person-options"><p><i class="fa fa-pencil" aria-hidden="true"></i></p><p><i class="fa fa-trash" aria-hidden="true"></i></p></div></li>');
+  $('#group-list').append(newPersonItem);
+}
 
-    initMap(center);
+$('.modal').modal();
 
-  });
-
-
-$(".modal").modal();
-
-$("#group-btn").on("click", function() {
-  $("#group-info").modal('open');
+$('#group-modal-btn').on('click', function() {
+  $('#group-info').modal('open');
 });
 
-$("#add-member-btn").on("click", function() {
-  $("#new-member-form").modal('open');
+$('#person-modal-btn').on('click', function() {
+  $('#new-member-form').modal('open');
+});
+
+$('#add-person-btn').on('click', function(e) {
+  e.preventDefault();
+
+  var name = $('#name-input').val().trim();
+  var address = $('#address-input').val().trim();
+
+  addPerson(name, address);
 });
